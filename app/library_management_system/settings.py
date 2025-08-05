b064283d907a747a5cce55ca9a77a68b704f4f74
taiga <taiga954@gmail.com>
2025-08-04 21:25:29 -0400

    Add Kubernetes and Helm setup

--- app/library_management_system/settings.py
+++ app/library_management_system/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(",")
 
 
 # Application definition
